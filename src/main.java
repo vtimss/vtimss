030a600725bd0fafd4ccb9d65b7f0684811d473a
vtimss <timofeevavaleria02@gmail.com>
2024-02-07 16:39:01 +0200

    first commit

--- src/main.java
+++ src/main.java
@@ -1,36 +1,26 @@
 public class main {
     public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6};
 
-        int number = 1;
+        insertionSort(array);
 
-        for (int i = 0; i < 55; i++) {
-            System.out.println(number + " ");
-
-            number += 2;
-    }
-    }
-    public static void task2(String[] args) {
-        for (int i = 90; i > 0; i = i - 5)
-            System.out.print(i + " ");
-    }
-    public static void task3(String[] args) {
-        for (int i = 0, a = 2; i < 20; i++) {
-            System.out.print(a + " ");
+        System.out.println("Sorted array: ");
+        for (int value : array) {
+            System.out.print(value + " ");
         }
     }
-    public static void task3(int a){
-        for (int i = -29; i < 10; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i)
+
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
             }
-        }
-    }
-    public static void task4(int a){
-        int sum = 0;
-        for (int i = 50; i < 150; i++) {
-            sum+=i;
-            System.out.println(sum);
-
+            array[j + 1] = key;
         }
     }
 }
